a811c5caf6da01cbe87eae60f27a2d152da02d53
ahmadrizqialfa <ahmadrizqialfa@yahoo.com>
2021-11-03 08:59:44 +0700

    migrations non relastion

---
+++ database/migrations/2021_11_02_230455_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id('activity_id');
+            $table->string('title');
+            $table->string('activity_image');
+            $table->text('notes');
+            $table->datetime('activity_date');
+            $table->integer('author_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
